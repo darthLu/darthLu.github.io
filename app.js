85c84e75f6c19a7e9c9954f9ab8398b1bb7593ad
darthLu <lindsey.wade@me.com>
2023-06-09 11:31:48 -0400

    added contect me form added footer

--- app.js
+++ app.js
@@ -22,4 +22,19 @@ function showSlides() {
 
 
 /* Contact Me Form Submission */
+window.addEventListener("load", function () {
+   const form = document.getElementById('contactMe');
+   form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+         method: 'POST',
+         body: data,
+      })
+         .then(() => {
+            alert("Success!");
+         })
+   });
+});
 
