f08f5b15f058b237d6787e3ed7eaf361ceabd67c
Lindsey Wade <lindsey.wade@me.com>
2023-06-13 21:47:12 -0400

    fixes for page update

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-/* Automatic Slide Show Script */
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-   let i = 0;
-   let slides = document.getElementsByClassName("mySlides");
-   let dots = document.getElementsByClassName("dot");
-   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-   }
-   slideIndex++;
-   if (slideIndex > slides.length) { slideIndex = 1 }
-   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-   }
-   slides[slideIndex - 1].style.display = "block";
-   dots[slideIndex - 1].className += " active";
-   setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-
 /* Contact Me Form Submission */
 window.addEventListener("load", function () {
    const form = document.getElementById('contactMe');
@@ -34,6 +11,7 @@ window.addEventListener("load", function () {
       })
          .then(() => {
             alert("Success!");
+            window.location.replace("https://www.lindseywade.me")
          })
    });
 });
